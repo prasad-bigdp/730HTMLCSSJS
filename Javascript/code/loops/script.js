74cf833735fc54476fbf023c4d928ba89605ae77
prasad-bigdp <bigdp585@gmail.com>
2025-03-21 08:10:25 +0530

    update

---
+++ Javascript/code/loops/script.js
@@ -0,0 +1,20 @@
+let i = 0; //initialization;
+while (i <= 10) // condition
+{
+    console.log(i);
+    i++; //updation
+}
+//using while loop print 20 to 1 in reverse
+i = 10;
+do
+{
+    console.log(i);
+    i--;
+} while (i >= 0);
+// using do while , try to print letters of a name in reverse
+//"PRASAD"==> D A S A R P
+
+
+
+
+
