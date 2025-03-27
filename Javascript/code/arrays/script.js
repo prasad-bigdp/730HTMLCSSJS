183431b5c582b39d847c440d210f92777fb900c8
prasad-bigdp <bigdp585@gmail.com>
2025-03-27 09:01:23 +0530

    update

--- Javascript/code/arrays/script.js
+++ Javascript/code/arrays/script.js
@@ -50,6 +50,83 @@ let z2 = arr.map(function (str) {
 console.log(z2) //[p,s,j]
 
 
+let arr1 = [5, 4, 6, 7];
+let x1=arr1.forEach(function (v,i)
+{
+    console.log(v); 
+    return v%2==0;
+})
+console.log(x1);//undefined
+let x2 = arr1.map(function (v, i) {
+	console.log(v)
+	return v % 2 == 0
+})
+console.log(x2) //[false,true,true,false]
+let x3 = arr1.filter(function (v, i) {
+	console.log(v)
+	return v % 2 == 0
+})
+console.log(x3) //[4,6]
+arr1=[5,15,4,7]
+//some
+let x4 = arr1.some((v,i) =>
+{
+    return v%2==0
+})
+console.log(x4)  //true -- atleast one retuned value should be true
+let x5 = arr1.every((v, i) => {
+	return v % 2 == 0
+})
+console.log(x5)  //false -- every returned value should be true
+//sorting:
+arr = [5, 4, 1, 6,16,44, 8,121,111111]
+let res = arr.sort();
+console.log(res)  //[1,11111,121,16,4,44,5,6,8]
+console.log(arr.sort((a, b) => a - b)); [1,4,5,6,8,16,44,121,11111]
+//Reduce
+//reduce is used for accumulated logic
+arr = [5, 4, 3, 1]
+let res2 = arr.reduce((p, c) => p + c,0);
+console.log(res2);
+arr = [1, 2, 3, 4, 5]
+let res3 = arr.reduce((p, c) => p * c, 1)
+console.log(res3)
+
+
+
+let obj = {
+	name: "raj",
+	age: 50,
+	fun: function () {
+		console.log(this.name)
+	},
+}
+obj.fun()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
