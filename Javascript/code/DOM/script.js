b58af87828379df1ede038c19b6297cc7da9c320
prasad-bigdp <bigdp585@gmail.com>
2025-03-28 09:05:28 +0530

    update

---
+++ Javascript/code/DOM/script.js
@@ -0,0 +1,61 @@
+// console.log(document)
+// console.log(document.getElementsByTagName('h1')[0])
+// document.getElementsByTagName("h1")[0].textContent = "Bye World"
+
+// document.getElementsByTagName('p')[0].textContent = "HEllo"
+
+// document.getElementsByClassName("bujji")[1].textContent = "😂(windows+.)"
+
+// document.getElementById('chitti').textContent = "bye bye"
+
+// document.querySelector('.bujji').textContent = "good morning"
+// console.log(document.querySelectorAll('.bujji'))
+
+// function increase ()
+// {
+//     let para = document.getElementById("chitti")
+// 		para.textContent = +para.textContent + 1
+// }
+
+let para = document.getElementById('count');
+let c = 0;
+function incr ()
+{
+  if (c < 20)
+  {
+    c++
+		para.textContent = c
+  }
+  else
+  {
+    alert("max limit reached")
+  }
+}
+function decr ()
+{
+  if (c > 0)
+  {
+    c--
+		para.textContent = c
+  }
+  else
+  {
+    alert("negitive values are not allowed")
+  }
+}
+function reset ()
+{
+  c = 0;
+  para.textContent = c;
+}
+
+
+
+
+
+
+
+
+
+
+
